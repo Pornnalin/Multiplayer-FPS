d27d310c1271d8713046c3c164c1c8e5b1982f26
MaiSang <51067238+Pornnalin@users.noreply.github.com>
2024-04-28 20:51:34 +0700

    add jump

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -11,14 +11,24 @@ public class PlayerController : MonoBehaviour
 
     public bool invertLook;
 
-    public float moveSpeed = 5f;
+    public float moveSpeed = 5f, runSpeed = 10f;
     private Vector3 moveDir;
     private Vector3 movement;
+    private float activeMoveSpeed;
 
     public CharacterController charCon;
+
+    private Camera cam;
+    public float jumpFore = 12f, gravityMod = 2.5f;
+
+    public Transform groundCheckPoint;
+    public bool isGround;
+    public LayerMask groundLayers;
+
     // Start is called before the first frame update
     void Start()
     {
+        cam = Camera.main;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -43,8 +53,42 @@ public class PlayerController : MonoBehaviour
             viewPoint.rotation = Quaternion.Euler(-verticalRotStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
         }
 
+
+
         moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        movement = ((transform.forward * moveDir.z) + (transform.right * moveDir.x)).normalized;
-        charCon.Move(movement * moveSpeed * Time.deltaTime);
+
+        //activeMoveSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : moveSpeed;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            activeMoveSpeed = runSpeed;
+        }
+        else
+        {
+            activeMoveSpeed = moveSpeed;
+        }
+
+        float yVel = movement.y;
+        movement = ((transform.forward * moveDir.z) + (transform.right * moveDir.x)).normalized * activeMoveSpeed;
+        movement.y = yVel;
+
+        if (charCon.isGrounded)
+        {
+            movement.y = 0f;
+        }
+        isGround = Physics.Raycast(groundCheckPoint.position, Vector3.down, .25f, groundLayers);
+
+        if (Input.GetButtonDown("Jump") && isGround)
+        {
+            movement.y = jumpFore;
+        }
+        movement.y += Physics.gravity.y * Time.deltaTime * gravityMod;
+        charCon.Move(movement * Time.deltaTime);
+    }
+
+    private void LateUpdate()
+    {
+        cam.transform.position = viewPoint.position;
+        cam.transform.rotation = viewPoint.rotation;
     }
 }
