eca09357142c427b366f0bd846cfe4f02cad8f25
MaiSang <51067238+Pornnalin@users.noreply.github.com>
2024-05-05 21:38:17 +0700

    shoot

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -24,7 +24,14 @@ public class PlayerController : MonoBehaviour
     public Transform groundCheckPoint;
     public bool isGround;
     public LayerMask groundLayers;
+    [Space]
+    public GameObject bulletImpact;
+    public float timeBetweenShot = .1f;
+    public float shorCounter;
 
+    public float maxHeat = 10f, heatPerShot = 1f, coolRate = 4f, overheatCoolRate = 5f;
+    [SerializeField] private float heatCounter;
+    [SerializeField] private bool overHeated;
     // Start is called before the first frame update
     void Start()
     {
@@ -36,8 +43,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
-
-
 
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
 
@@ -52,8 +57,6 @@ public class PlayerController : MonoBehaviour
         {
             viewPoint.rotation = Quaternion.Euler(-verticalRotStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
         }
-
-
 
         moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
@@ -84,8 +87,77 @@ public class PlayerController : MonoBehaviour
         }
         movement.y += Physics.gravity.y * Time.deltaTime * gravityMod;
         charCon.Move(movement * Time.deltaTime);
+
+        if (!overHeated)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Shoot();
+            }
+            if (Input.GetMouseButton(0))
+            {
+                shorCounter -= Time.deltaTime;
+
+                if (shorCounter <= 0)
+                {
+                    Shoot();
+                }
+
+            }
+            heatCounter -= coolRate * Time.deltaTime;
+
+        }
+        else
+        {
+            heatCounter -= overheatCoolRate * Time.deltaTime;
+            if (heatCounter <= 0)
+            {
+                // heatCounter = 0;
+                overHeated = false;
+            }
+        }
+        if (heatCounter < 0)
+        {
+            heatCounter = 0;
+        }
+
+
+        CursorUpdate();
     }
 
+    private static void CursorUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else if (Cursor.lockState == CursorLockMode.None && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+
+        }
+    }
+
+    private void Shoot()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(.5f, .5f, 0f));
+        ray.origin = cam.transform.position;
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Debug.Log("We hit" + hit.collider);
+            GameObject bulletImapactObject = Instantiate(bulletImpact, hit.point + (hit.normal * 0.002f), Quaternion.LookRotation(hit.normal, Vector3.up));
+            Destroy(bulletImapactObject, 10f);
+            //hit.normal: เป็นเวกเตอร์ที่ตั้งฉากกับพื้นผิวที่ชน นี่มักถูกใช้เพื่อหาทิศทางของการชน
+        }
+        shorCounter = timeBetweenShot;
+        heatCounter += heatPerShot;
+        if (heatCounter >= maxHeat)
+        {
+            heatCounter = maxHeat;
+            overHeated = true;
+        }
+
+    }
     private void LateUpdate()
     {
         cam.transform.position = viewPoint.position;
